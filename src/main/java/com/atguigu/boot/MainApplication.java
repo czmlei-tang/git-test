01d0086d25fa6feb2217ce362d70831e23e3744d
Mrtang <2987894459@qq.com>
2022-05-06 18:23:52 +0800

    mater commit1

--- src/main/java/com/atguigu/boot/MainApplication.java
+++ src/main/java/com/atguigu/boot/MainApplication.java
@@ -14,6 +14,7 @@ public class MainApplication {
         SpringApplication.run(MainApplication.class, args);
         System.out.println("three");
         System.out.println("hot-fix");
+        System.out.println("master1");
 
     }
 }
